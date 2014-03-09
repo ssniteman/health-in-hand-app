1d1eecb18d52c354b7022bc49641d4f7bdbcd44b
Shane Sniteman <ssniteman@gmail.com>
2014-03-09 18:26:25 -0400

    creating template

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,13 @@ $('[rel="popover"]').popover({
     trigger: 'hover',
     html: true
 });
+
+// User input renders on last page
+
+$('.orderbtn').click(function() {
+
+    var userinput = $('.options-userinput').val();
+
+
+
+})
